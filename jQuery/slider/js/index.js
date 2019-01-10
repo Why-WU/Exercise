3413cf935f801ae6110ebf2a474764b2da46ad02
‘Why-WU’ <wuhanyu001@126.com>
2019-01-10 16:54:43 +0800

    练习

---
+++ jQuery/slider/js/index.js
@@ -0,0 +1,12 @@
+$.fn.extend({
+  slider: function(obj) {
+    var defaultset = {
+      time: 1000,
+      pic: []
+    };
+    var nweSet = $.extend({}, defaultset, obj);
+
+    var content = $('<div class = "slider-panner"></div>');
+    var imgBar = '<div></div>'
+  }
+});
