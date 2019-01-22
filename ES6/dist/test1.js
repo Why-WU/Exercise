5a26c111df41c95e76d23bc4dfce983c3ebcdd37
‘Why-WU’ <wuhanyu001@126.com>
2019-01-22 15:01:34 +0800

    admin

---
+++ ES6/dist/test1.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var a = 10;
+var foo = function foo(a, b) {
+  return a + b;
+}; //等同于 let foo = (a,b) => {return(a+b)}
+var b = 20;
---
+++ ES6/src/test1.js
@@ -0,0 +1,3 @@
+let a = 10;
+let foo = (a,b) => a+b //等同于 let foo = (a,b) => {return(a+b)}
+let b = 20;
