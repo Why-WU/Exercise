7cacb1a4ece4874896ab18de58a15b191b434813
‘Why-WU’ <wuhanyu001@126.com>
2019-02-14 16:30:07 +0800

    vueapp

---
+++ vueappdemo/src/components/Navigator.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <header></header>
+    <nav>
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuList: [
+        {
+          name: "聊天",
+          path: "/talk"
+        },
+        {
+          name: "电影",
+          path: "/movie"
+        }
+      ]
+    };
+  }
+};
+</script>
