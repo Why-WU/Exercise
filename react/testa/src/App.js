d30390a9e61f0646ba2969cea1f6b28e9e0af9f2
‘Why-WU’ <wuhanyu001@126.com>
2019-02-25 12:05:44 +0800

    react

--- react/testa/src/App.js
+++ react/testa/src/App.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Class1 from "./components/Class1";
+import Class2 from "./components/Class2";
+import Class3 from "./components/Class3";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
 class App extends Component {
   render() {
-
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <div>
+            <Link to="/">Class1</Link>|
+            <Link to="/class2">Class2</Link>|
+            <Link to="/class3">Class3</Link>
+            <Switch>
+              <Route path={"/"} component={Class1} />
+              <Route path={"/class2"} component={Class2} />
+              <Route path={"/class3"} component={Class3} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
