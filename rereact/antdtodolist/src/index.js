09485470a0dc2ce64f6aba970cb554377724c8f1
‘Why-WU’ <wuhanyu001@126.com>
2019-09-12 23:28:06 +0800

    antd

---
+++ rereact/antdtodolist/src/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: Harry.Wu
+ * @Date: 2019-09-09 00:32:02
+ * @LastEditors: Harry.Wu
+ * @LastEditTime: 2019-09-12 22:44:51
+ * @Description: 
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TodoList from './TodoList';
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
+
