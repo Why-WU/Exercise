c2cf37a6451f75a09979cdb2d3ffa033ffc8a792
Why-WU <wuhanyu001@126.com>
2019-09-18 17:16:18 +0800

    todolistui

--- rereact/antdtodolist/src/TodoList.js
+++ rereact/antdtodolist/src/TodoList.js
@@ -2,16 +2,16 @@
  * @Author: Harry.Wu
  * @Date: 2019-09-12 22:32:32
  * @LastEditors: Harry.Wu
- * @LastEditTime: 2019-09-15 12:59:14
+ * @LastEditTime: 2019-09-18 16:13:07
  * @Description:
  */
 import React, { Component } from "react";
 import "antd/dist/antd.css";
-import { Input } from "antd";
-import { Button, List } from "antd";
+
 import store from "./store/index";
 import { getInputChangeAction } from "./store/actionCreators";
-import Test from "./test/test";
+
+import TodoListUI from "./TodoListUI";
 
 import {
   CHANGE_INPUT_VALUE,
@@ -27,36 +27,19 @@ class TodoList extends Component {
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleStoreChange = this.handleStoreChange.bind(this);
     this.handleBtnClick = this.handleBtnClick.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
     store.subscribe(this.handleStoreChange);
   }
 
   render() {
     return (
-      <div style={{ marginTop: "10px", marginLeft: "10px" }}>
-        <Test></Test>
-        <div>
-          <Input
-            placeholder="hello"
-            value={this.state.inputValue}
-            style={{ width: "300px", marginRight: "10px"}}
-            onChange={this.handleInputChange}
-            id='div1'
-          ></Input>
-          <Button type="primary" onClick={this.handleBtnClick}>
-            提交
-          </Button>
-        </div>
-        <List
-          style={{ marginTop: "10px", width: "300px" }}
-          bordered
-          dataSource={this.state.list}
-          renderItem={(item, index) => (
-            <List.Item onClick={this.handleItemDelete.bind(this, index)}>
-              {item}
-            </List.Item>
-          )}
-        />
-      </div>
+      <TodoListUI
+        inputValue={this.state.inputValue}
+        handleInputChange={this.handleInputChange}
+        handleBtnClick={this.handleBtnClick}
+        list={this.state.list}
+        handleItemDelete={this.handleItemDelete}
+      />
     );
   }
 
--- rereact/antdtodolist/src/TodoListUI.js
+++ rereact/antdtodolist/src/TodoListUI.js
@@ -2,7 +2,45 @@
  * @Author: Harry.Wu
  * @Date: 2019-09-16 12:24:33
  * @LastEditors: Harry.Wu
- * @LastEditTime: 2019-09-16 12:24:33
- * @Description: 
+ * @LastEditTime: 2019-09-18 16:12:49
+ * @Description:
  */
 import React, { Component } from "react";
+import { Button, List ,Input } from "antd";
+
+class TodoListUI extends Component {
+  render() {
+    return (
+      <div style={{ marginTop: "10px", marginLeft: "10px" }}>
+        <div>
+          <Input
+            placeholder="hello"
+            value={this.props.inputValue}
+            style={{ width: "300px", marginRight: "10px" }}
+            onChange={this.props.handleInputChange}
+            id="div1"
+          ></Input>
+          <Button type="primary" onClick={this.props.handleBtnClick}>
+            提交
+          </Button>
+        </div>
+        <List
+          style={{ marginTop: "10px", width: "300px" }}
+          bordered
+          dataSource={this.props.list}
+          renderItem={(item, index) => (
+            <List.Item
+              onClick={index => {
+                this.props.handleItemDelete(index);
+              }}
+            >
+              {item}
+            </List.Item>
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+export default TodoListUI;
